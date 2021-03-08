81d399c973a82feb3ef6895bded4f9fc6f1c408c
Zachary Mitchell <31785316+zachMitchell@users.noreply.github>
2021-03-08 13:06:27 -0500

    snap I posted the token -_-

--- token.js
+++ token.js
@@ -1,2 +1,2 @@
 //The most important piece: your authentication token. Use this so that your discord bot can log into it's account
-module.exports = 'NzY2MTE0MTA0NzcxMjE1Mzkw.X4epXA.sWWUrZjXqWwz_GKpe6nyvHhjFDQ';
+module.exports = '';
